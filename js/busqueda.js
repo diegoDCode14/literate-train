2d82a363fb1ab9cc62b7adebf829f28924ba6033
Diego Duarte Fernández <diegoduarte100@hotmail.com>
2019-07-29 10:43:35 -0500

    Agruegué carpetas de js, imagenes y css

---
+++ js/busqueda.js
@@ -0,0 +1,27 @@
+var buscador=document.getElementById('buscar');
+var btnBuscar=document.getElementById('btnBuscador');
+var popup2=document.getElementById('popup2');
+var overlay2=document.getElementById('overlay2');
+var cerrar2=document.getElementById('btn-cerrar-popup2');
+var termin=document.getElementById('termino');
+
+btnBuscador.addEventListener('click',function(){
+  if(buscador.value=="Muebles" || buscador.value=="muebles"){
+      location.href="muebles.html";
+  }else if(buscador.value=="Electrónicos" || buscador.value=="electrónicos" || buscador.value=="electronicos"
+  || buscador.value=="Electronicos"){
+      location.href="electro.html";
+}else if (buscador.value=="Moda" || buscador.value=="moda"){
+  location.href="moda.html";
+}else if(buscador.value=="Lectura" || buscador.value=="lectura"){
+  location.href="lectura.html"
+}else{
+    overlay2.classList.add('active');
+    popup2.classList.add('active');
+}
+});
+cerrar2.addEventListener('click',function(){
+  overlay2.classList.remove('active');
+  popup2.classList.remove('active');
+  buscador.value="";
+});
---
+++ js/todo.js
@@ -0,0 +1,14 @@
+var overlay=document.getElementById('overlay');
+var cerrar=document.getElementById('btn-cerrar-popup');
+var popup=document.getElementById('popup');
+var cuerpo=document.getElementById('cuerpo');
+/*boton*/
+var abrir=document.getElementById('btn-gallery');
+abrir.addEventListener('click',function(){
+  overlay.classList.add('active');
+  popup.classList.add('active');
+});
+cerrar.addEventListener('click',function(){
+  overlay.classList.remove('active');
+  popup.classList.remove('active');
+});
